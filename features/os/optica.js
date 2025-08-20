f12aa5a13ac50286fe7d4bb809a797621b68731f
lufekura-1 <lufekura@gmail.com>
2025-08-20 14:52:12 -0300

    feat: add optica service order template

---
+++ features/os/optica.js
@@ -0,0 +1,42 @@
+// Configuração da O.S. para Óptica
+
+export function makeCodigo(numero, siglaPerfil){
+  return `OT${numero}${siglaPerfil}`;
+}
+
+export const corPrincipal = '#8B0000'; // vermelho escuro
+
+export const dadosGerais = {
+  nome: '',
+  telefone: '',
+  cpf: '',
+  armacao: '',
+  lente: '',
+  grau: {
+    OE: { esf: '', cil: '', eixo: '', dnp: '', adicao: '' },
+    OD: { esf: '', cil: '', eixo: '', dnp: '', adicao: '' }
+  },
+  observacao: '',
+  previsaoEntrega: ''
+};
+
+export const viaLoja = {
+  valorArmacao: 0,
+  valorLente: 0,
+  get valorTotal(){
+    return this.valorArmacao + this.valorLente;
+  }
+};
+
+export const garantia = {
+  visivelEm: ['cliente', 'loja'],
+  texto: '' // texto definido pelo administrador
+};
+
+export default {
+  makeCodigo,
+  corPrincipal,
+  dadosGerais,
+  viaLoja,
+  garantia
+};
